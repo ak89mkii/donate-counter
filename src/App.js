6e87bef7829e9d2db8b5d772d08fe354103d49c3
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2021-06-05 19:26:39 -0400

    Added title, total, and date sections.

--- src/App.js
+++ src/App.js
@@ -7,40 +7,43 @@ import './App.css';
 
 class Home extends Component {
   constructor() {
-      super()
-      this.state={time: new Date()}
+    super()
+    this.state={time: new Date()}
   }
   
-    render() {
-        return (
-            <div>
-                <Segment>
-                  <Grid divided='vertically'>
-                    <Grid.Column>
-                      <h1 className='header'>Donation Tracker Application</h1>
-                      <h2 className='start'>Total Donations</h2>
-                    </Grid.Column>
-                  </Grid>
-                  <Grid relaxed='very' columns={2} divided>
-                    <Grid.Row>
-                    <Grid.Column>
-                      <h2 className='start'>Start Date</h2>
-                      <div className='value'>
-                        <p>Date Placeholder</p>
-                      </div>
-                    </Grid.Column>
-                    <Grid.Column>
-                      <h2 className='start'>End Date</h2>
-                      <div className='value'>
-                        <p>Date Placeholder</p>
-                      </div>                    
-                    </Grid.Column>
-                    </Grid.Row>
-                  </Grid>
-                </Segment>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Segment>
+          <Grid divided='vertically'>
+            <Grid.Column>
+              <h1 className='header'>Donation Tracker Application</h1>
+              <h2 className='start'>Total Donations</h2>
+              <div className='value'>
+                <p>Date Placeholder</p>
+              </div>    
+            </Grid.Column>
+          </Grid>
+          <Grid relaxed='very' columns={2} divided>
+            <Grid.Row>
+              <Grid.Column>
+                <h2 className='start'>Start Date</h2>
+                <div className='value'>
+                  <p>Date Placeholder</p>
+                </div>
+              </Grid.Column>
+              <Grid.Column>
+                <h2 className='start'>End Date</h2>
+                <div className='value'>
+                  <p>Date Placeholder</p>
+                </div>                    
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+      </div>
+    )
+  }
 }
 
 export default Home
